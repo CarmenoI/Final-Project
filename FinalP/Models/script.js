2c36b0f3dfb3970c4d5969e5fc2eb89b1b8ce2af
Carmeno Innocent <43419707+CarmenoI@users.noreply.github.com>
2018-11-29 16:11:05 -0500

    Add files via upload

---
+++ FinalP/Models/script.js
@@ -0,0 +1,44 @@
+// fetch("")
+//     .then(res => res.json())
+//
+//     const submit = document.getElementById('submit')
+//     const select = document.getElementById('select')
+//
+//     let apiURL = ''
+//       fetch (apiURL)
+//       .then(res => res.json()) // parse response as JSON (can be res.text() for plain response)
+//       .then(response =>
+//       let companyInfo(response.network[0].href) );
+//       for{ let i=0; i = response.network.legth; i++){
+//
+//         let str = ' '
+//       }
+//         if(typeof response.network[0].company)
+//       }
+//         if(response.network[i].company.legnth){
+//           for (let j=0; j < response.networks[i].company .length; j++)(
+//             str += response.networks[i].company[j]
+//           }
+//         }
+//         let text = document.createTextNode(str)
+//         let option = document.createText
+//     }
+//     domCity
+//     country
+//
+
+var value=["1","2","3","4","5"]
+
+function getUnique(count){
+  let next = value.slice(value);
+  let rtn = [];
+
+  for (var i=0; 1<count; i++){
+    let index = Math.floor(Math.random()next.length);
+    let removed=next .splice (index, 1);
+    rtn.push (removed [0]);
+  }
+  }
+  console.log("---");
+  console.log(get unique(3));
+};
